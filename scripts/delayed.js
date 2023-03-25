fd05dad70298288d041ec312e7bfbf8b1d00a896
sandesh-sinha <sandesh85210@gmail.com>
2023-03-25 20:37:58 +0530

    fix: removes multiple startCarousel calls

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,38 +6,42 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-function playVideo(divWrappingVideo, currentPlaying) {
+function playVideo(divWrappingVideo) {
     divWrappingVideo.firstElementChild.muted = true;
     setTimeout(() => {
         divWrappingVideo.firstElementChild.play();
     }, 1000);
 }
 
-function stopVideo(divWrappingVideo, currentPlaying) {
+function stopVideo(divWrappingVideo) {
     divWrappingVideo.firstElementChild.pause();
     divWrappingVideo.firstElementChild.load();
 }
 
-function deactivateCurrentElement(divWrapping, currentPlaying){
-    divWrapping.classList.remove('active');
+function deactivateCurrentElement(divWrapping){
+    if(divWrapping.classList.contains('active')) {
+        divWrapping.classList.remove('active');
+    }
     if(divWrapping.querySelectorAll('img').length === 0){
-        stopVideo(divWrapping, currentPlaying);
+        stopVideo(divWrapping);
     }
 }
 
-function activateNextElement(divWrapping, currentPlaying){
-    divWrapping.classList.add('active');
+function activateNextElement(divWrapping){
+    if(divWrapping.classList.contains('active')) divWrapping.classList.add('active');
     if(divWrapping.querySelectorAll('img').length === 0){
-        playVideo(divWrapping, currentPlaying);
+        playVideo(divWrapping);
     }
 }
+
+let carouselInterval;
 
 function startCarousel(sequence) {
     let currentPlaying = 0;
     const switchTimeout = 10000;
     sequence.classList.add('idle');
     playVideo(sequence.children[currentPlaying]);
-    setInterval(() => {
+    carouselInterval = setInterval(() => {
         deactivateCurrentElement(sequence.children[currentPlaying], currentPlaying);
         currentPlaying += 1;
         if(currentPlaying === sequence.childElementCount){
@@ -56,11 +60,18 @@ function idleHandler(){
     });
 }
 
+function stopCarousel(sequence) {
+    sequence.classList.remove('idle');
+    sequence.children.forEach((sequenceElement) => {
+        deactivateCurrentElement(sequenceElement);
+    })
+}
+
 function interactionEventHandler(){
     const carousels = document.querySelectorAll('.sequence');
     carousels.forEach((sequence) => {
         if (sequence.classList.contains('idle')) {
-            sequence.classList.remove('idle');
+            stopCarousel(sequence);
         }
     });
 }
