f859cb087ef655890aef28ce582d9252e909b2a1
sandesh-sinha <sandesh85210@gmail.com>
2023-03-25 18:07:06 +0530

    fix: add images support

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,19 +1,29 @@
+function createVideoElement(url, type) {
+  const media = document.createElement('video');
+  media.classList.add('sequence-element-video');
+  const source = document.createElement('source');
+  source.src = url;
+  source.type = type;
+  media.append(source);
+  return media;
+}
+
 export default function decorate(block) {
   const carouselElements = [...block.children];
   const sequence = document.createElement('div');
   sequence.classList.add('sequence');
   carouselElements.forEach((carouselElement) => {
-    const url = carouselElement.children[0].textContent;
     const type = carouselElement.children[1].textContent;
     const sequenceElement = document.createElement('div');
     sequenceElement.classList.add('sequence-element', 'fadeIn');
-    const media = document.createElement('video');
-    media.classList.add('sequence-element-video');
-    // add source to video element
-    const source = document.createElement('source');
-    source.src = url;
-    source.type = type;
-    media.append(source);
+    let media; 
+    if(type === 'img') {
+      media = carouselElement.querySelector('img');
+    }
+    else {
+      const url = carouselElement.children[0].textContent;
+      media = createVideoElement(url, type);
+    }
     sequenceElement.append(media);
     sequence.append(sequenceElement);
   })
